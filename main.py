3ecc96431e2d204c2154750e901b954ead0131f0
Zach-Balhous <zacharybalhous@gmail.com>
2024-10-15 13:53:02 -0400

    Done

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 3# Put your function here
-
+def factorial(num):
+    if num > 1:
+        num * factorial(num - 1)
+    
+    if num == 1:
+        return(1)
+    
 
 # testing
-num = 5
+num = int(input())
 print(factorial(num))
